607f6cdf1bea7310e3f4c3a76030686e96b1eaf1
kaanyagci <kaan.yagci@makepad.fr>
2023-03-28 00:30:35 +0200

    :sparkles: Session structure created and exported cookies converted to http.Cookie

--- utils.go
+++ utils.go
@@ -1,7 +1,9 @@
 package gogpt
 
 import (
+	"github.com/playwright-community/playwright-go"
 	"math/rand"
+	"net/http"
 	"time"
 )
 
@@ -11,4 +13,34 @@ const baseURL = "https://chat.openai.com"
 func randomTimeOut() float64 {
 	r := rand.New(rand.NewSource(time.Now().UnixMicro()))
 	return float64(r.Intn(10000) + 1000)
+}
+
+func playwrightSameSiteAttributeToHttpSameSite(attribute *playwright.SameSiteAttribute) http.SameSite {
+	switch attribute {
+	case playwright.SameSiteAttributeStrict: return http.SameSiteStrictMode
+	case playwright.SameSiteAttributeLax: return http.SameSiteLaxMode
+	case playwright.SameSiteAttributeNone: return http.SameSiteNoneMode
+	default: return http.SameSiteDefaultMode
+	}
+}
+
+func playwrightToHttpCookie (playwrightCookie *playwright.BrowserContextCookiesResult) *http.Cookie {
+	return &http.Cookie{
+		Name:       playwrightCookie.Name,
+		Value:      playwrightCookie.Value,
+		Path:       playwrightCookie.Path,
+		Domain:     playwrightCookie.Domain,
+		Expires:    time.Unix(int64(playwrightCookie.Expires), 0),
+		Secure:     playwrightCookie.Secure,
+		HttpOnly:   playwrightCookie.HttpOnly,
+		SameSite:   playwrightSameSiteAttributeToHttpSameSite(&playwrightCookie.SameSite),
+	}
+}
+
+func playwrightCookiesToHttpCookies(cookies []*playwright.BrowserContextCookiesResult) []*http.Cookie {
+	var result []*http.Cookie = make([]*http.Cookie, len(cookies), len(cookies))
+	for i,cookie := range cookies {
+		result[i] = playwrightToHttpCookie(cookie)
+	}
+	return result
 }
