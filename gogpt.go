dfa3afd23677b78df1052ba47e0f2e0464634d94
kaanyagci <kaan.yagci@makepad.fr>
2023-03-28 15:57:24 +0200

    feat: gpt structure extended

--- gogpt.go
+++ gogpt.go
@@ -72,8 +72,7 @@ func New(browserContextPath string, headless, debug bool) (GoGPT,error) {
 		browserContextPath: browserContextPath,
 		browser:            browser,
 		page:               page,
-		// TODO: User should be nil at the beginning
-		user:               Session{},
+		session:            nil,
 	}, nil
 }
 
