0882ea00a335765d12d51e20fb074e742acf7676
kaanyagci <kaan.yagci@makepad.fr>
2023-03-29 14:15:17 +0200

    feat: user session initialization implemented

--- session.go
+++ session.go
@@ -1,13 +1,49 @@
 package gogpt
 
 import (
-	"net/http"
-	"net/url"
+	"encoding/json"
+	"time"
 )
 
+const sessionExpirationTimeLayout = "2006-01-02T15:04:05.999Z"
+
+
+type User struct {
+	ID        string `json:"id"`
+	Name      string `json:"name"`
+	Email     string  `json:"email"`
+	Image     string  `json:"image"`
+	Picture   string  `json:"picture"`
+	Groups    []string `json:"groups"`
+}
+
 type Session struct {
-	Cookies []*http.Cookie
-	AuthToken string
-	Email string
-	Avatar url.URL
+	User       User `json:"user"`
+	Expires    string `json:"expires"`
+	AccessToken string `json:"accessToken"`
+}
+
+//unmarshalGPTSessionResponseJSON returns a pointer to a Session from the jsonData passed in parameters
+//it returns an error if something goes wrong while unmarshalling the json
+func unmarshalGPTSessionResponseJSON(jsonData []byte) (*Session, error) {
+	var result Session
+	err := json.Unmarshal(jsonData, &result)
+	if err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+
+//isExpired function verifies if the current session is expired by using its Expires attribute
+//if returns an error if the Expires string can not be pased using sessionExpirationTimeLayout
+func (s *Session) isExpired() (bool,error) {
+	expirationTime , err := time.Parse(sessionExpirationTimeLayout, s.Expires)
+	if err != nil {
+		return true,err
+	}
+	if expirationTime.Before(time.Now()) {
+		return true, nil
+	}
+	return false, nil
 }
