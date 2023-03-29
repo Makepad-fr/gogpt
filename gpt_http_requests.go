e8b1cc79004232efeb0be5ace673e030dc4ed118
kaanyagci <kaan.yagci@makepad.fr>
2023-03-29 14:15:17 +0200

    feat: user session initialization implemented

--- gpt_http_requests.go
+++ gpt_http_requests.go
@@ -2,7 +2,7 @@ package gogpt
 
 import (
 	"go.uber.org/zap"
-	"io/ioutil"
+	"io"
 	"net/http"
 )
 
@@ -27,7 +27,9 @@ func (g *gpt) initCookieJarAndHttpClient() error {
 	return nil
 }
 
-func (g *gpt) loadSessionDetails() error{
+//initSession initializes the session of the current gpt instance.
+//It returns an error if something goes wrong while unmarshalling the api response
+func (g *gpt) initSession() error{
 	err := g.cookieJar.setExpiredCookies()
 	if err != nil {
 		return err
@@ -39,11 +41,54 @@ func (g *gpt) loadSessionDetails() error{
 	defer resp.Body.Close()
 
 	// Read the response body
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return err
 	}
-	// TODO: Parse the body as session
 	logger.Debug("session token response", zap.String("session-request-response", string(body)))
+	s, err := unmarshalGPTSessionResponseJSON(body)
+	if err != nil {
+		logger.Error("Error while unmarshalling session response")
+		return err
+	}
+	g.session = s
+
+	return nil
+}
+
+
+//refreshSession verifies if there's a session exists. If there's no session exists, creates one using initSession
+//if there's an existing session verifies if the session is expired using isExpired function. If the session is expired
+// recreates the session using initSession
+func (g *gpt) refreshSession() error {
+	if g.session == nil {
+		return g.initSession()
+	}
+	isExpired, err := g.session.isExpired()
+	if err != nil {
+		logger.Error("Error while checking if the existing session is expired", zap.String("expiration-date-string", g.session.Expires))
+		return g.initSession()
+	}
+	if isExpired {
+		return g.initSession()
+	}
+	return nil
+}
+
+//prepareRequest prepares the cookies and the user session to use in each http request. This function should be called
+//before each http request to ensure that the request will not be blocked
+func (g *gpt) prepareRequest() error {
+	err := g.initCookieJarAndHttpClient()
+	if err != nil {
+		return err
+	}
+	err = g.cookieJar.setExpiredCookies()
+	if err != nil {
+		return err
+	}
+	err = g.refreshSession()
+	if err != nil {
+		return err
+	}
 	return nil
 }
