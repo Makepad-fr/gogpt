939ce36a7e0722cee1845239f170ca31ae2d252e
kaanyagci <kaan.yagci@clique.paris>
2023-04-05 11:50:11 +0200

    :sparkles: Getting user chat history implemented
    a new structure called Conversation implemented. And HTTP reqest to get first page of the user's conversation is also implemented.

---
+++ conversation.go
@@ -0,0 +1,16 @@
+package gogpt
+
+type ConversationsResponse struct {
+	Items                   []Conversation `json:"items"`
+	Total                   int            `json:"total"`
+	Limit                   int            `json:"limit"`
+	Offset                  int            `json:"offset"`
+	HasMissingConversations bool           `json:"has_missing_conversations"`
+}
+
+type Conversation struct {
+	ID         string `json:"id"`
+	Title      string `json:"title"`
+	CreateTime string `json:"create_time"`
+	UpdateTime string `json:"update_time"`
+}
